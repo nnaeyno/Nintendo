1e9c1b3feb26ef1ded1cba0f179856257cb3d3c9
nnaeyno <ngogo20@freeuni.edu.ge>
2023-07-01 16:19:02 +0400

    PPU background rendering part 1

--- PPU.h
+++ PPU.h
@@ -3,25 +3,83 @@
 #include "cartridge.h"
 
 using namespace std;
+
 class PPU {
     private:		
 	    uint8_t nameTable[2][Cartridge::ONE_KILOBYTE];  // two kilobytes 
         uint8_t	paletteTable[32]; //colors
         uint8_t patternTable[2][Cartridge::FOUR_KILOBYTES];
-        olc::Pixel screen[64]; //olc::Pixel is sourced from olcPixelGameEngine
-
-
+        
+        //olc::Pixel is sourced from olcPixelGameEngine
+        olc::Pixel screen[64]; //palScreen
+        //sprite is foreground stuff
+        olc::Sprite* spritePatterns[2]; //sprPatternTable
+        olc::Sprite* spriteScreen; //sprScreen
+	    olc::Sprite* spriteNames[]; //sprNameTable
         shared_ptr<Cartridge> cart;
         int16_t scanline = 0;
 	    int16_t cycle = 0;
-	    
+
+        // understand the syntax and what does it do 
+        union
+        {
+            struct
+            {
+                uint8_t unused : 5;
+                uint8_t sprite_overflow : 1;
+                uint8_t sprite_zero_hit : 1;
+                uint8_t vertical_blank : 1;
+            };
+
+            uint8_t reg;
+        } status;
+
+
+        union
+        {
+            struct
+            {
+                uint8_t grayscale : 1;
+                uint8_t render_background_left : 1;
+                uint8_t render_sprites_left : 1;
+                uint8_t render_background : 1;
+                uint8_t render_sprites : 1;
+                uint8_t enhance_red : 1;
+                uint8_t enhance_green : 1;
+                uint8_t enhance_blue : 1;
+            };
+
+            uint8_t reg;
+        } mask;
+
+        union PPUCTRL
+        {
+            struct
+            {
+                uint8_t nametable_x : 1;
+                uint8_t nametable_y : 1;
+                uint8_t increment_mode : 1;
+                uint8_t pattern_sprite : 1;
+                uint8_t pattern_background : 1;
+                uint8_t sprite_size : 1;
+                uint8_t slave_mode : 1; // unused
+                uint8_t enable_nmi : 1;
+            };
+
+            uint8_t reg;
+        } control;
+
+	    uint8_t address_latch = 0x00; // low or high can be an enum would be cleaner
+	    uint8_t ppu_data_buffer = 0x00;
+        uint16_t ppu_addr = 0x00;
     public:
         PPU();
         ~PPU();
 
         static const int MAX_PPU = 0x3FFF;
         static const int MIN_PPU = 0x2000;
-
+        static const int TILE_SIZE = 16;
+        static const int PIXEL_SIZE = 8;
         uint8_t read(uint16_t addr, bool RDONLY=false); //CPURead 
         uint8_t ppuRead(uint16_t addr, bool RDONLY=false);
 
@@ -30,5 +88,8 @@ class PPU {
 
         void connectCartridge(const std::shared_ptr<Cartridge>& cartridge);
 	    void clock(); //count one system tick 
+        bool nmi; //needs reset
 
+        olc::Sprite& getPatterns(uint8_t palette, uint8_t ind); //olc::Sprite& olc2C02::GetPatternTable(uint8_t i, uint8_t palette)
+        olc::Pixel& getColourFromRam(uint8_t palette, uint8_t pix);
 };
